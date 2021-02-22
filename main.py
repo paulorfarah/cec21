4f2414fa12be43db8c7a5bdd541020b38d48e65e
Paulo Farah <paulorfarah@gmail.com>
2021-02-22 14:13:27 -0300

    uncomment main

--- main.py
+++ main.py
@@ -16,19 +16,19 @@ def main():
     # elbow method
     calculate_centroids(x)
 
-    # kmeans_7_10 = 37 #cluster column numbers in results.csv
-    # for i in [kmeans_7_10]:
-    #     algoritm = df.columns[[i]].tolist()[0].replace('\\\\', '\\')
-    #     fname = algoritm.replace('\\', '_')
-    #     pca(fname, df, features, algoritm, title)
-    #     count_compositions(df, algoritm, features)
-    #
-    #     csv_files = ['50_kmeans_7_10.csv']
-    #     clusters = [7]
-    #     method_names = ['km']
-    #
-    #     for csv_file, cluster, method in zip(csv_files, clusters, method_names):
-    #         plot_combinations(csv_file, method, cluster)
+    kmeans_7_10 = 37 #cluster column numbers in results.csv
+    for i in [kmeans_7_10]:
+        algoritm = df.columns[[i]].tolist()[0].replace('\\\\', '\\')
+        fname = algoritm.replace('\\', '_')
+        pca(fname, df, features, algoritm, title)
+        count_compositions(df, algoritm, features)
+
+        csv_files = ['50_kmeans_7_10.csv']
+        clusters = [7]
+        method_names = ['km']
+
+        for csv_file, cluster, method in zip(csv_files, clusters, method_names):
+            plot_combinations(csv_file, method, cluster)
 
 
 if __name__ == "__main__":
